ab9fd517089ba622ac2acb4f72940da054b5c9a9
Simone Pierazzini <makeroo@gmail.com>
2019-01-06 08:18:41 +0100

    cleanup

--- taxi_scout_frontend/src/reducers/index.js
+++ taxi_scout_frontend/src/reducers/index.js
@@ -1,16 +1,3 @@
-/*
-copiato dal tutorial redux...
-const initialState = {
-    // TODO: my properties
-};
-
-const rootReducer = (state = initialState, action) => state;
-
-export default rootReducer;
-*/
-
-
-
 import { combineReducers } from 'redux';
 import { excursion } from "./excursion";
 import { account } from "./account";
