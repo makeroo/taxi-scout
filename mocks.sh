f534bddbe77add30767a1fd5c1025617e305ae08
Simone Pierazzini <makeroo@gmail.com>
2019-05-02 08:23:52 +0200

    configuration.go mocks

--- mocks.sh
+++ mocks.sh
@@ -6,4 +6,4 @@ CMD_HOME="$(dirname "$COMMAND")"
 cd "${CMD_HOME}"
 
 mockgen -destination=mocks/mock_dao.go -package=mocks -source=storage/dao.go
-mockgen -destination=mocks/mock_configuration.go -package=mocks -source=rest_backend/configuration.go
+mockgen -destination=mocks/mock_configuration.go -package=mocks -source=rest/configuration.go
