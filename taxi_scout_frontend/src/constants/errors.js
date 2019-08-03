64d7de93e8be078fdcca43efcbabca822e1ea066
Simone Pierazzini <makeroo@gmail.com>
2019-08-03 06:40:05 +0200

    unauthorized error code

--- taxi_scout_frontend/src/constants/errors.js
+++ taxi_scout_frontend/src/constants/errors.js
@@ -3,4 +3,5 @@ export const NOT_FOUND = 'not_found';
 export const EXPIRED = 'expired';
 export const SERVICE_NOT_AVAILABLE = 'service_not_available';
 export const BAD_REQUEST = 'bad_request';
+export const NOT_AUTHORIZED = 'not_authorized';
 //export const FORBIDDEN = 'forbidden';
